557c147f065d246b45c7ceffce84e1452361919f
Phu Huynh <phu.huynh@internetbrands.com>
2023-06-15 16:21:00 -0700

    Updated main.py to retrieve text-chunks from DB

--- medscape/custom_config.py
+++ medscape/custom_config.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append( "/var/www/html/medscape/medscape-content-feed/linear_model" )
+sys.path.append( os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))) )
 
 from config import config
 environment = 'local'
--- medscape/main.py
+++ medscape/main.py
@@ -1,5 +1,12 @@
 # This is a version of the main.py file found in ../../../server/main.py for testing the plugin locally.
 # Use the command `poetry run dev` to run this.
+
+import os
+import sys
+sys.path.append( os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '/medscape' )
+from custom_config import utils
+
+
 from typing import Optional
 import uvicorn
 from fastapi import FastAPI, File, Form, HTTPException, Body, UploadFile
@@ -45,10 +52,10 @@ async def get_manifest(request):
     return FileResponse(file_path, media_type="text/json")
 
 
-@app.route("/.well-known/logo.png")
+@app.route("/.well-known/medscape-logo.webp")
 async def get_logo(request):
-    file_path = "./local-server/logo.png"
-    return FileResponse(file_path, media_type="text/json")
+    file_path = "./medscape/medscape-logo.webp"
+    return FileResponse(file_path, media_type="image/x-png")
 
 
 @app.route("/.well-known/openapi.yaml")
@@ -99,12 +106,43 @@ async def upsert(
         raise HTTPException(status_code=500, detail="Internal Service Error")
 
 
-@app.post("/query", response_model=QueryResponse)
-async def query_main(request: QueryRequest = Body(...)):
+@app.post(
+    "/query",
+    response_model=QueryResponse,
+)
+async def query_main(
+    request: QueryRequest = Body(...),
+):
     try:
         results = await datastore.query(
             request.queries,
         )
+        ### custom medscape ###
+        # add text chunks
+        article_ids = []
+        chunks = []
+        for query in results:
+            for chunk in query.results:
+                article_id, chunk = chunk.id.split('_')
+                article_ids.append(article_id)
+                chunks.append(chunk)
+
+        if len(article_ids):
+            db = utils.db_connection()
+            query = "select article_id, chunk, text from poc_gpt_qna_article_embeddings where article_id in (%s) and chunk in (%s)" % (
+                utils.implode(',', article_ids),
+                utils.implode(',', chunks)
+            )
+            df_chunks = utils.read_sql_inmem_uncompressed(query, db)
+            if not df_chunks.empty:
+                for query in results:
+                    for chunk in query.results:
+                        article_id, _chunk = chunk.id.split('_')
+                        df_chunk = df_chunks[(df_chunks['article_id']==int(article_id)) & (df_chunks['chunk']==int(_chunk))]
+                        if not df_chunk.empty:
+                            chunk.text = df_chunk.iloc(0)[0].text
+
+        ### custom medscape ###
         return QueryResponse(results=results)
     except Exception as e:
         print("Error:", e)
@@ -142,4 +180,4 @@ async def startup():
 
 
 def start():
-    uvicorn.run("local-server.main:app", host="localhost", port=PORT, reload=True)
+    uvicorn.run("local-server.main:app", host="0.0.0.0", port=PORT, reload=True)
